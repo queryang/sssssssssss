47a54bda8cd90fddb05df0b4a07a5bb4f7a4aba3
queryang <query_wy@ooutlook.com>
2020-12-04 14:18:17 +0800

    文件服务器修改的还挺满意

--- storage-server/src/main/java/sg/storage/web/FileUploadController.java
+++ storage-server/src/main/java/sg/storage/web/FileUploadController.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.repository.query.Param;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -20,18 +22,19 @@ import sg.storage.common.util.EtagUtil;
 import sg.storage.common.util.ResultModelUtil;
 import sg.storage.model.dto.FileByte;
 import sg.storage.model.vo.file.FileRequest;
-
 import sg.storage.model.vo.file.FileUploadResponse;
 import sg.storage.service.IFileUploadService;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
+/**
+ * @author WuYang
+ */
 @Slf4j
 @CrossOrigin
 @RestController
 @Api(tags = "文件上传API")
-@RequestMapping(value = "/api/file")
+@RequestMapping(value = "/file")
 public class FileUploadController {
 
     @Autowired
@@ -42,55 +45,18 @@ public class FileUploadController {
     @Autowired
     @Qualifier(value = "ossUploadService")
     private IFileUploadService ossUploadService;
-
-//    public static final String SCS_URL = "http://192.168.3.128/scs/fault/storage";
-//    public static final String SCS_URL_LOCAL = "http://localhost/scs/fault/storage";
-    public static final String SCS_PATH = "H:\\scs\\";
-//    @ApiOperation(value = "上传文件", notes = "上传文件")
-//    @PostMapping(produces = "application/json;charset=UTF-8")
-//    public ResultModel<FileUploadResponse> upload(
-//            @ApiParam(value = "appKey", required = true) String appKey
-//            , @ApiParam(value = "ownId", required = true) String ownId
-//            , @ApiParam(value = "备注") String remark
-//            , @ApiParam(value = "文件夹id") String folderId
-//            , @ApiParam(value = "上传人") String uploadUser
-//            , HttpServletRequest request) {
-//        if (StringUtils.isBlank(appKey)) {
-//            return ResultModelUtil.getFailInstance(CodeEnum.NO_PARAM, "未获取到appKey");
-//        }
-//        if (StringUtils.isBlank(ownId)) {
-//            return ResultModelUtil.getFailInstance(CodeEnum.NO_PARAM, "未获取到ownId");
-//        }
-//        FileRequest fileRequest = new FileRequest();
-//        fileRequest.setAppKey(appKey);
-//        fileRequest.setOwnId(ownId);
-//        fileRequest.setRemark(remark);
-//        fileRequest.setFolderId(folderId);
-//        fileRequest.setUploadUser(uploadUser);
-//        ResultModel<FileUploadResponse> resultModel = fileReceive(fileRequest, request);
-//        List<FileResponse> successFiles = resultModel.getData().getSuccessFiles();
-//        //存储文件信息容器
-//        Map<String,String> map = new HashMap<>();
-//        //获取文件信息
-//        for (FileResponse successFile : successFiles) {
-//            String filePath = successFile.getFilePath();
-//            map.put(successFile.getOriginalFileName(),filePath);
-//        }
-//
-//        JSONObject jsonObject = (JSONObject)JSONObject.toJSON(map);
-//        String s = HttpUtils.sendPostJson(SCS_URL_LOCAL, jsonObject);
-//        System.out.println(s);
-//        return resultModel;
-//    }
+    //配置文件中自己定义问价存储位置
+    @Value("${file.scsFilePath}")
+    private String scsFilePath;
 
 
-    @ApiOperation(value = "修改后上传文件", notes = "修改后上传文件")
-    @PostMapping(produces = "application/json;charset=UTF-8")
-    public ResultModel<FileUploadResponse> uploadStorage(HttpServletRequest request, @ApiParam(value = "type",required = true)String type) {
+    @ApiOperation(value = "上传文件-需要指定文件的业务逻辑folder是什么", notes = "上传文件")
+    @PostMapping(value = "/scs/{folder}",produces = "application/json;charset=UTF-8")
+    public ResultModel<FileUploadResponse> uploadStorage(HttpServletRequest request, @PathVariable("folder") String folder) {
 
         FileRequest fileRequest = new FileRequest();
         //加入预存路径
-        fileRequest.setPath(SCS_PATH+type);
+        fileRequest.setPath(scsFilePath+folder);
         ResultModel<FileUploadResponse> resultModel = fileReceive(fileRequest, request);
 
         return resultModel;
@@ -153,59 +119,6 @@ public class FileUploadController {
         }
     }
 
-//    /**
-//     * 文件接收
-//     *
-//     * @param fileRequest
-//     * @param request
-//     * @return
-//     */
-//    @RequestMapping("/bzxx")
-//    private ResultModel<FileUploadResponse> fileReceiveBzxx(FileRequest fileRequest, HttpServletRequest request) {
-//        List<FileByte> fileBytes = getFileByte(fileRequest, request);
-//        if (CollectionUtils.isEmpty(fileBytes)) {
-//            return ResultModelUtil.getFailInstance(CodeEnum.NO_PARAM, "未获取到文件");
-//        }
-//        /**
-//         * 若文件id为空，则为新增文件
-//         * 若文件id不为空，则为更新文件，更新文件仅可一份文件
-//         */
-//        if (StringUtils.isBlank(fileRequest.getId())) {
-//            switch (fileProperties.getLocation()) {
-//                case FileConstant.LOCAL:
-//                    //本地存储
-//                    return localUploadService.receiveAndAdd(fileBytes);
-//                case FileConstant.OSS:
-//                    return ossUploadService.receiveAndAdd(fileBytes);
-//                default:
-//                    return ResultModelUtil.getFailInstance(CodeEnum.NO_PARAM, "未获取到存储方式");
-//            }
-//        }
-//        FileByte fileByte = fileBytes.get(0);
-//        fileByte.setId(fileRequest.getId());
-//        switch (fileProperties.getLocation()) {
-//            case FileConstant.LOCAL:
-//                return localUploadService.receiveAndUpdate(fileByte);
-//            case FileConstant.OSS:
-//                return ossUploadService.receiveAndUpdate(fileByte);
-//            default:
-//                return ResultModelUtil.getFailInstance(CodeEnum.NO_PARAM, "未获取到存储方式");
-//        }
-//    }
-
-    //        //todo 以下是通信主服务的请求
-//        List<FileResponse> successFiles = resultModel.getData().getSuccessFiles();
-//        //存储文件信息容器
-//        Map<String,String> map = new HashMap<>();
-//        //获取文件信息
-//        for (FileResponse successFile : successFiles) {
-//            String filePath = successFile.getFilePath();
-//            map.put(successFile.getOriginalFileName(),filePath);
-//        }
-//
-//        JSONObject jsonObject = (JSONObject)JSONObject.toJSON(map);
-//        String s = HttpUtils.sendPostJson(SCS_URL_LOCAL, jsonObject);
-//        System.out.println(s);
 
 
     /**
@@ -219,9 +132,7 @@ public class FileUploadController {
         List<FileByte> fileBytes = new ArrayList<>(1);
         //todo这里做了修改
         MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) request;
-        //天马行空的解决办法--暂时抛弃
-//        MultipartResolver resolver = new CommonsMultipartResolver(request.getSession().getServletContext());
-//        MultipartHttpServletRequest multipartHttpServletRequest = resolver.resolveMultipart(request);
+
         EtagUtil etagUtil = new EtagUtil();
         Iterator<String> fileNames = multipartHttpServletRequest.getFileNames();
         while (fileNames.hasNext()) {
--- storage-server/src/test/java/sg/storage/PmApplicationTests.java
+++ storage-server/src/test/java/sg/storage/PmApplicationTests.java
@@ -2,6 +2,7 @@ package sg.storage;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PmApplicationTests {
 
+    @Value("${file.scsFilePath}")
+    private String value;
+
     @Test
     public void contextLoads() {
+
+
+        System.out.println(value);
     }
 
 }
